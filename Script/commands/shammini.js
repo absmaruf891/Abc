778b195db3fdce502be256f903a6274ab9f081ef
absmaruf891 <absmaruf891@gmail.com>
2025-05-15 02:11:12 +0600

    Update and rename video.js to shammini.js

--- Script/commands/shammini.js
+++ Script/commands/shammini.js
@@ -20,7 +20,7 @@ function deleteAfterTimeout(filePath, timeout = 5000) {
 
 module.exports = {
   config: {
-    name: "music",
+    name: "Shammini",
     version: "2.0.2",
     hasPermssion: 0,
     credits: "RAHAT",
